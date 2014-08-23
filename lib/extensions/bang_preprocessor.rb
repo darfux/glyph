ca5c176b908d01893e3a6d71bf658916a7308df1
darfux <darfux@163.com>
2014-08-23 16:14:59 +0800

    prototype of coffeescript's package control on rails finished

--- lib/extensions/bang_preprocessor.rb
+++ lib/extensions/bang_preprocessor.rb
@@ -1,7 +1,7 @@
 module BangBang
   class Processor < Sprockets::DirectiveProcessor
     def prepare
-      if file.include? app_asset_path and file.include? 'coffee' # avoid modifying rails default files
+      if file.include? app_asset_path and file.include? '.coffee' # avoid modifying rails default files
         handle_import(file.include? app_asset_path+app_name)
       end
       super
@@ -25,11 +25,35 @@ module BangBang
           # end
           tmp_data<<l
         end
-        tmp = requires.join + fetch_statements.join # ensure taht the requires are on the top of file
-        @data = tmp+tmp_data
-        unless withbag
-          @data += "\n#-nobag\n"
+        tmp = requires.join + fetch_statements.join # ensure that the requires are on the top of file
+        @data = (
+          if withbag
+            internal_statements = handle_internal
+            tmp+internal_statements+tmp_data
+          else
+            tmp+tmp_data+"\n#-nobag\n"
+          end
+        )
+      end
+      def handle_internal
+        current_file_name = file.split('/')[-1]
+        # dir = Dir.new()
+        internal_klasses = []
+        orgdir = Dir.pwd
+        Dir.chdir file.split('/')[0..-2].join('/')
+        package = file.split(app_asset_path)[1].split('/')[0..-2].join('.')
+        Dir.foreach('.') do |f|
+          next if File.directory?(f)
+          next if f==current_file_name
+          internal_klasses.push f.split('.')[0].classify
         end
+        statements = "\n"+internal_klasses.join(' = ')+" = null\n"
+        statements<<"$(document).ready ->\n"
+        internal_klasses.each do |k|
+          statements<<"\t#{k} = fetch('#{package}.#{k}')\n"
+        end
+        statements<<"\n"
+        statements
       end
       def app_name
         @app_name ||= Rails.application.class.to_s.split("::").first.downcase
